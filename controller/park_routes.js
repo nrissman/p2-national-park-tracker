7b5cd563d55d836774ad7a7cd441d8f427be18f8
nrissman <nrissman90@gmail.com>
2022-07-14 16:48:51 -0400

    index is working

--- controller/park_routes.js
+++ controller/park_routes.js
@@ -7,25 +7,31 @@ const router = express.Router()
 require('dotenv').config()
 // const Park =require(".controller")
 const axios = require('axios')
+const api_key = process.env.API_KEY
 
 
+///////////////SHOW ROUTE///////////////////////////
+router.get('/:parkCode', (req, res) => { 
+    const pc = req.params.parkCode
+    //test using park code acad
+    //url for testing should be /parks/acad
+    axios.get(`https://developer.nps.gov/api/v1/parks?parkCode=${pc}&api_key=${api_key}`)
+        .then(apiRes => {
+            const park = apiRes.data.data[0]
+            console.log('this is the response from the api', park.fullName)
+            res.render('parks/show', { park })
+        })
 
-
-
-
-router.get('/', (req, res) => {
-    const api_key = process.env.API_KEY
-    axios.get(`https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${api_key}`)
-
-    .then(apiResponse => {
-        const parks = apiResponse.data
-        console.log(apiResponse.data)
-        res.render('parks/index', { parks })
-    })
-    
+        .catch(err=>{
+            console.error('Error:', err)
+            res.json(err)
+        })
     
     
 })
+
+
+
 
 
 // router.get('/', (req, res) => {
